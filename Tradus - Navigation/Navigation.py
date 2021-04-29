dda19533879b4b7a1d6ae14ca1148aafa49cbb7b
humblethinker <43913473+humblethinker@users.noreply.github.com>
2021-04-29 21:55:17 +0530

    Add files via upload

---
+++ Tradus - Navigation/Navigation.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome('C:\\Users\\humbl\\Downloads\\chromedriver_win32\\chromedriver.exe')
+
+driver.maximize_window()
+driver.get("https://tradus.com/")
+
+links = set()
+elems = driver.find_elements_by_xpath("//a[@href]")
+
+open('Output1.txt', 'w').close()
+
+text_file = open('Output1.txt', "a")
+
+for elem in elems:
+    link = str(elem.get_attribute("href"))
+    if "https" in link:
+        links.add(link)
+
+no = 0
+
+for link in links:
+    no=no+1
+    text_file.write("Link "+str(no)+": " +str(link)+"\n\n")
+
+text_file.close()
+
+driver.quit()
+
