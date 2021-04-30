55401265134d444636ad6e1adec0ff57008b5312
humblethinker <43913473+humblethinker@users.noreply.github.com>
2021-04-30 12:12:26 +0530

    Add files via upload

---
+++ Swayam - Load testing/locustfile.py
@@ -0,0 +1,11 @@
+import time
+from locust import HttpUser, task, between
+
+class QuickstartUser(HttpUser):
+    wait_time = between(1, 2.5)
+
+    @task
+    def hello_world(self):
+        self.client.get("/about")
+        self.client.get("/explorer")
+        self.client.get("/nc_details")
