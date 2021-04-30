84a3c65a182ee42184dc5953fbee71dcf425509b
humblethinker <43913473+humblethinker@users.noreply.github.com>
2021-04-30 12:03:33 +0530

    Add files via upload

---
+++ Tradus - Load testing/locustfile.py
@@ -0,0 +1,14 @@
+import time
+from locust import HttpUser, task, between
+
+class QuickstartUser(HttpUser):
+    wait_time = between(1, 2.5)
+
+    @task
+    def hello_world(self):
+        self.client.get("/account/inbox")
+        self.client.get("/account")
+        self.client.get("/en")
+
+    def on_start(self):
+        self.client.post("/account/login#tab-login", json={"username":"ewfrtrtjje@sammdmk.ac.in", "password":"Abcdefgh"})
